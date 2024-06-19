d77678e48cfa0df57e0face3320a57ec0af3ff38
JoaoGuilhermeVaz <162444627+JoaoGuilhermeVaz@users.noreply.github.com>
2024-06-19 09:56:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let topmod = 0;
+let leftmod = 0;
+
+$("body").on("mousemove", webhandler);
+
+function WebHandler(event){
+    let button = $('#no').position();
+    let buttoncenter = {
+        x: button.left + 50,
+        y: button.top + 20,
+    }
+}
